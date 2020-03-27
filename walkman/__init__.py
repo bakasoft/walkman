d447c5a6253dec45494d07cc5b214c07379623d7
sergiouph <sergio.pedraza@wizeline.com>
2020-03-26 21:48:14 -0600

    Fix flake8 warnings

--- walkman/__init__.py
+++ walkman/__init__.py
@@ -1,5 +1,5 @@
-from ._impl import WalkmanException
-from ._impl import ParsingError
-from ._impl import Location
-from ._impl import LocationArea
-from ._impl import Source
+from ._impl import WalkmanException  # noqa: F401
+from ._impl import ParsingError  # noqa: F401
+from ._impl import Location  # noqa: F401
+from ._impl import LocationArea  # noqa: F401
+from ._impl import Source  # noqa: F401
